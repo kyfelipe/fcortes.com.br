1f7174be5d107edc29aafe4ab67ab4afced93840
Felipe Côrtes <contatos.cortes@gmail.com>
2020-05-06 22:28:44 -0300

    Finalizado visual do Sobre mim e da página 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,6 +13,25 @@ const NotFoundPage = () => (
     <S.NotFoundWrapper>
       <S.Title>404</S.Title>
       <S.SubTitle>Página não encontrada</S.SubTitle>
+      <S.Coffee>
+        <S.CoffeeHeader>
+          <S.CoffeeHeaderButtons className="one" />
+          <S.CoffeeHeaderButtons className="two" />
+          <S.CoffeeHeaderButtonsDisplay />
+          <S.CoffeeHeaderButtonsDetails />
+        </S.CoffeeHeader>
+        <S.CoffeeBody>
+          <S.CoffeeBodyExit />
+          <S.CoffeeBodyArm />
+          <S.CoffeeBodyLiquid />
+          <S.CoffeeBodySmoke className="one" />
+          <S.CoffeeBodySmoke className="two" />
+          <S.CoffeeBodySmoke className="three" />
+          <S.CoffeeBodySmoke className="four" />
+          <S.CoffeeBodySmokeCup />
+        </S.CoffeeBody>
+        <S.CoffeeFooter />
+      </S.Coffee>
       <S.BackHome to="/">Voltar ao início</S.BackHome>
     </S.NotFoundWrapper>
   </Layout>
--- src/pages/sobre-mim.js
+++ src/pages/sobre-mim.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../layouts/page'
 import SEO from '../components/seo'
@@ -8,6 +9,20 @@ import Social from '../components/Social'
 import * as S from '../styles/about-me'
 
 const SobreMim = () => {
+  const { authorImage } = useStaticQuery(
+    graphql`
+      query {
+        authorImage: file(relativePath: { eq: "profile.png" }) {
+          childImageSharp {
+            fixed(width: 130, height: 130) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `
+  );
+
   return (
     <Layout>
       <SEO
@@ -16,14 +31,28 @@ const SobreMim = () => {
         keywords={[`felipe`, `felipe côrtes`, `blog`, `devops`]}
       />
       <Main>
-        <S.TitleWrapper>
-          <S.Title>Sobre Mim</S.Title>
+        <S.Title>Sobre Mim</S.Title>
+        <S.HeadWrapper>
+          <S.ProfileImageWrapper>
+            <S.ProfileImage fixed={authorImage.childImageSharp.fixed} />
+          </S.ProfileImageWrapper>
           <S.SocialAboutMe>
             <Social />
           </S.SocialAboutMe>
-        </S.TitleWrapper>
-        <S.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</S.Text>
-        <S.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</S.Text>
+          <S.TextHeaderWrapper>
+            <S.AuthorText>Felipe Côrtes, 21 anos </S.AuthorText>
+            <S.Text>Estudante de Sistemas de Informação, com previsão de conclusão para 2021. Amo e atuo no setor de DevOps, atualmente como Estagiário na Vertigo Tecnologia. Também possuo interesse em Angular e Java, tendo exercido a função de estagiário na equipe de desenvolvimento da Credilink.</S.Text>
+          </S.TextHeaderWrapper>
+        </S.HeadWrapper>
+        <S.Text>Me considero comunicativo, proativo, trabalho bem em equipe e anseio por aprimorar cada vez mais as minhas habilidades e aptidões. Criei este blog com o objetivo de compartilhar meus conhecimentos sobre DevOps de maneira descomplicada e direta para quem estuda, trabalha ou é entusiasta pela área.</S.Text>
+        <S.Text className="bold">Minhas principais habilidades são:</S.Text>
+        <S.ListWrapper>
+          <S.ListItem>AWS (Amazon Web Services)</S.ListItem>
+          <S.ListItem>Docker</S.ListItem>
+          <S.ListItem>Kubernetes</S.ListItem>
+          <S.ListItem>Gitlab CI/CD</S.ListItem>
+          <S.ListItem>Terraform</S.ListItem>
+        </S.ListWrapper>
       </Main>
     </Layout>
   )
